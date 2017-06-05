ef570ab15a9e19e74b3b08c6427926f1252e86c0
James Nikolaidis <dimitriosnikolaidis@hotmail.com>
2017-06-05 11:15:19 +0300

    update first screen , delete button insert spinner

--- app/src/main/java/voxxed_days_greece/voxxeddays/Screens/First_Screen.java
+++ app/src/main/java/voxxed_days_greece/voxxeddays/Screens/First_Screen.java
@@ -1,24 +1,28 @@
 package voxxed_days_greece.voxxeddays.Screens;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.Time;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import voxxed_days_greece.voxxeddays.ALERT_DIALOGS.PassedAthensDate;
+import voxxed_days_greece.voxxeddays.ADAPTERS.state_spinner;
 import voxxed_days_greece.voxxeddays.R;
 
 /**
@@ -28,62 +32,72 @@ import voxxed_days_greece.voxxeddays.R;
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class First_Screen extends AppCompatActivity {
 
-    @BindView(R.id.VoxxedDaysAthens)
-    Button mVoxxedAthensButton;
-
-    @BindView(R.id.VoxxedDaysThessaloniki)
-    Button mVoxxedThessalonikiButton;
-
-
     private  Intent nextScreen;
     private SimpleDateFormat DateFormat=null;
     private Date getCurrentDate=null;
-    private GregorianCalendar gregorianCalendar;
+    private Activity mActivity=null;
+    private SharedPreferences mSharedPreference=null;
+    private SharedPreferences.Editor editor=null;
+    private static ArrayList<Drawable> images = new ArrayList<>();
+    public static String STATE_SELECTION="state_selection";
+    public static String STATE_NUMBER="state_number";
 
-    Time today;
+
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spot_layout);
-
         ButterKnife.bind(this);
-
         getCurrentDate = new Date();
-        gregorianCalendar = new GregorianCalendar(TimeZone.getTimeZone("Europe/Athens"));
-        today= new Time(Time.getCurrentTimezone());
-        today.setToNow();
-    }
+        mActivity = this;
+        mSharedPreference = getSharedPreferences("state_selection",MODE_PRIVATE);
+        editor = mSharedPreference.edit();
 
 
-    //create on click listener for the button on this layout
-    @OnClick({R.id.VoxxedDaysAthens,R.id.VoxxedDaysThessaloniki})
-    public void sent_on_the_voxxed_days_screens(View view){
-
-
-            //check if the clicked button is the thessaloniki button
-            if(view.getId()==R.id.VoxxedDaysThessaloniki){
-                     nextScreen  = new Intent(this,thessaloniki_main_screen.class);
-                     this.startActivity(nextScreen);
-            }else{
-                //check if the clicked button is the Athens button
-                        //check if its in the Athens Voxxed Period or else it will show a dialog to choose past events
-                     if(getCurrentDate.getMonth()==5){
-                         nextScreen  = new Intent(this,athens_main_screen.class);
-                         this.startActivity(nextScreen);
-                     }else{
-
-                         PassedAthensDate athens_main_screen = new PassedAthensDate(getApplicationContext());
-                         athens_main_screen.createDialog(getApplicationContext(),this,"");
-                     }
-            }
 
     }
 
 
 
+    @OnClick(R.id.StateSpinner)
+    public void SpinnerTextViewClick(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        alertDialog.setContentView(R.layout.state_spinner_item);
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        ListView listView = (ListView) alertDialog.findViewById(R.id.SpinnerListView);
+        listView.setAdapter(new state_spinner(getApplicationContext(),getResources().getStringArray(R.array.State_List),this, returnImagesId(this)));
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+              editor.putInt("state_number",position);
+              editor.commit();
+              nextScreen = new Intent(getApplicationContext(),thessaloniki_main_screen.class);
+              startActivity(nextScreen);
+
+
+         }
+        });
+
+    }
+
+
+
+
+    public static ArrayList<Drawable> returnImagesId(Activity activity){
+
+        images.add(activity.getDrawable(R.drawable.white_tower));
+        images.add(activity.getDrawable(R.drawable.acropolis_ico));
+        images.add(activity.getDrawable(R.drawable.volos3));
+        images.add(activity.getDrawable(R.drawable.patra_ico));
+        images.add(activity.getDrawable(R.drawable.crete_ico_v2));
+        images.add(activity.getDrawable(R.drawable.piraeus_ico));
+        return images;
+    }
 
 
 

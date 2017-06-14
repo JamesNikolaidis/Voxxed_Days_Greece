5b9c9fd2287fd6f93a5cb8dbb47af211936457d7
James Nikolaidis <dimitriosnikolaidis@hotmail.com>
2017-06-14 21:40:58 +0300

    change name and some parts of the code

--- app/src/main/java/voxxed_days_greece/voxxeddays/Screens/main_screen.java
+++ app/src/main/java/voxxed_days_greece/voxxeddays/Screens/main_screen.java
@@ -1,11 +1,11 @@
 package voxxed_days_greece.voxxeddays.Screens;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,10 +15,25 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import voxxed_days_greece.voxxeddays.R;
+import voxxed_days_greece.voxxeddays.adapters.speakers_adapter;
+import voxxed_days_greece.voxxeddays.api.get_speakers;
+import voxxed_days_greece.voxxeddays.models.speakers;
 
 public class main_screen extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -26,29 +41,39 @@ public class main_screen extends AppCompatActivity
 
     TextView mStateName;
     private SharedPreferences sharedPreferences;
-
-
+    private ViewFlipper mViewFlipper;
+    private ScrollView scrollView;
+    private Timer mTime = new Timer();
+    private LinearLayout mRelative;
+    private Button mFloatingButton;
+    private TextView mWelcomeTitle=null,mWelcomeBody=null;
+    public static Activity mActivity=null;
+    private View theView;
+    private Timer checkforchanges=new Timer();
+    private LinearLayout mSpeakersLayout=null;
+    private ListView mSpeaker1=null,mSpeaker2=null;
+    private get_speakers get_speakers = null;
+    private ArrayList<speakers> speakerses1=new ArrayList<>(),speakerses2=new ArrayList<>();
+    private ArrayList<String> speakersPictures=new ArrayList<>();
+    private ArrayList<String> speakersName=new ArrayList<>();
+    private ArrayList<String> speakersPictures1=new ArrayList<>();
+    private ArrayList<String> speakersName1=new ArrayList<>();
+    private speakers_adapter adapter,adapter1;
+    private  SharedPreferences.Editor editor;
+    View speaker;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        mActivity = this;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         sharedPreferences = getSharedPreferences(First_Screen.STATE_SELECTION,MODE_PRIVATE);
         setTitle(R.string.app_name);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -58,6 +83,12 @@ public class main_screen extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        theView = inflater.inflate(R.layout.nav_header_main,null);
+
+
         drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
@@ -66,7 +97,7 @@ public class main_screen extends AppCompatActivity
 
             @Override
             public void onDrawerOpened(View drawerView) {
-                SetTitle((TextView)drawerView.findViewById(R.id.voxxed_state_name),(ImageView)drawerView.findViewById(R.id.stateImageView));
+                SetNavTitle((TextView)drawerView.findViewById(R.id.voxxed_state_name),(ImageView)drawerView.findViewById(R.id.stateImageView));
 
             }
 
@@ -81,11 +112,33 @@ public class main_screen extends AppCompatActivity
             }
         });
 
-        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View vi = inflater.inflate(R.layout.nav_header_main, null); //log.xml is your file.
+
+        mViewFlipper = (ViewFlipper)findViewById(R.id.imageSlider);
+        mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out));
+        mRelative = (LinearLayout)findViewById(R.id.main_screen_relative);
+        mFloatingButton = (Button)findViewById(R.id.fab);
+
+        mFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        changeImage();
+        mWelcomeTitle = (TextView)findViewById(R.id.message_title);
+        mWelcomeBody = (TextView)findViewById(R.id.message_body);
 
 
 
+       BackGroundStaff backGroundStaff = new BackGroundStaff();
+       backGroundStaff.execute();
+
+
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
+        get_speakers = get_speakers.get_speakersObject();
+        editor= sharedPreferences.edit();
     }
 
     @Override
@@ -126,21 +179,23 @@ public class main_screen extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        /*
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.speakers) {
+            DestroyFlipperAndTimer();
+            setSpeakerListViews();
+            Updates();
+        } else if (id == R.id.sessions) {
+            DestroyFlipperAndTimer();
+        } else if (id == R.id.keynotes) {
+            DestroyFlipperAndTimer();
+        } else if (id == R.id.voting) {
+            DestroyFlipperAndTimer();
         } else if (id == R.id.nav_share) {
-
+            DestroyFlipperAndTimer();
         } else if (id == R.id.nav_send) {
-
+            DestroyFlipperAndTimer();
         }
-*/
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
@@ -154,34 +209,58 @@ public class main_screen extends AppCompatActivity
         switch (sharedPreferences.getInt(First_Screen.STATE_NUMBER,-1)){
 
             case 0:
+                setTitle(R.string.app_name_thessaloniki);
+                break;
+            case 1:
+                setTitle(R.string.app_name_athens);
+                break;
+            case 2:
+                setTitle(R.string.app_name_volos);
+                break;
+            case 3:
+                setTitle(R.string.app_name_patra);
+                break;
+            case 4:
+                setTitle(R.string.app_name_crete);
+                break;
+            case 5:
+                setTitle(R.string.app_name_piraeus);
+                break;
+            default:
+                setTitle("Error");
+                break;
+
+
+        }
+    }
+
+    public void SetNavTitle(TextView stateName,ImageView imageView){
+
+        switch (sharedPreferences.getInt(First_Screen.STATE_NUMBER,-1)){
+
+            case 0:
                 imageView.setImageResource(R.drawable.voxxed_days_thessaloniki_menu_icon);
                 stateName.setText(R.string.app_name_thessaloniki);
-                setTitle(R.string.app_name_thessaloniki);
                 break;
             case 1:
                 imageView.setImageResource(R.drawable.acropolis_ico);
                 stateName.setText(R.string.app_name_athens);
-                setTitle(R.string.app_name_athens);
                 break;
             case 2:
                 imageView.setImageResource(R.drawable.volos3);
                 stateName.setText(R.string.app_name_volos);
-                setTitle(R.string.app_name_volos);
                 break;
             case 3:
                 imageView.setImageResource(R.drawable.patra_ico);
                 stateName.setText(R.string.app_name_patra);
-                setTitle(R.string.app_name_patra);
                 break;
             case 4:
                 imageView.setImageResource(R.drawable.crete_ico_v2);
                 stateName.setText(R.string.app_name_crete);
-                setTitle(R.string.app_name_crete);
                 break;
             case 5:
                 imageView.setImageResource(R.drawable.piraeus_ico);
                 stateName.setText(R.string.app_name_piraeus);
-                setTitle(R.string.app_name_piraeus);
                 break;
             default:
                 setTitle("Error");
@@ -193,6 +272,197 @@ public class main_screen extends AppCompatActivity
 
 
 
+    public void changeImage(){
+            mTime.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                   runOnUiThread(new Runnable() {
+                       @Override
+                       public void run() {
+                           mViewFlipper.showNext();
+                       }
+                   });
+                }
+            },0,10000);
+    }
 
+    public void DestroyFlipperAndTimer(){
+            mRelative.removeAllViews();
+            mRelative.setBackgroundColor(Color.parseColor("#867979"));
+            LayoutInflater inflater1 = LayoutInflater.from(this);
+            speaker = inflater1.inflate(R.layout.speakers,null);
+            mRelative.addView((LinearLayout)speaker.findViewById(R.id.speakers_linear_layout));
+            StopTime();
+    }
+
+    public void StopTime(){
+        mTime.cancel();
+    }
+    public void ResumeTime(){mTime = new Timer();}
+
+
+
+
+
+
+    public class BackGroundStaff extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            fetchData();
+
+
+            if(sharedPreferences.getInt(First_Screen.STATE_NUMBER,-1)==0){
+                //is thessaloniki
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWelcomeTitle.setText(mActivity.getResources().getString(R.string.state_thessaloniki_welocome));
+                        mWelcomeBody.setText(mActivity.getResources().getString(R.string.thessaloniki_welcome_message));
+                    }
+                });
+
+            }else if(sharedPreferences.getInt(First_Screen.STATE_NUMBER,-1)==1){
+                //is Athens
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWelcomeTitle.setText(mActivity.getResources().getString(R.string.state_athens_welcome));
+                        mWelcomeBody.setText(mActivity.getResources().getString(R.string.athens_welcome_message));
+                    }
+                });
+            }else{
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWelcomeTitle.setText("You do not find information about this state");
+                        mWelcomeBody.setText("You do not find information about this state");
+                    }
+                });
+            }
+
+
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    SetTitle((TextView)theView.findViewById(R.id.voxxed_state_name),(ImageView)theView.findViewById(R.id.stateImageView));
+                }
+            });
+
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        StopTime();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ResumeTime();
+        changeImage();
+    }
+
+
+    public void check_orientation(){
+
+       // Configuration.ORIENTATION_LANDSCAPE;
+    }
+
+
+
+    public void setSpeakerListViews(){
+
+
+        mSpeaker1 = (ListView) findViewById(R.id.speakers_list_1);
+        mSpeaker2 = (ListView) findViewById(R.id.speakers_list_2);
+        adapter=new speakers_adapter(getApplicationContext(),speakersPictures,speakersName);
+        adapter1=new speakers_adapter(getApplicationContext(),speakersPictures1,speakersName1);
+        mSpeaker1.setAdapter(adapter);
+        mSpeaker2.setAdapter(adapter1);
+        this.setTitle("Our Amazing Speakers");
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speakersName.clear();
+        speakersName1.clear();
+        speakersPictures.clear();
+        speakersPictures1.clear();
+        get_speakers.clearSpeakersList();
+        checkforchanges.cancel();
+    }
+
+    public  void fetchData(){
+        ArrayList<speakers> speakerses = get_speakers.returnSpeakersList();
+        int counter=0;
+
+        while(counter!=(speakerses.size()/2)){
+            speakerses1.add(speakerses.get(counter));
+            speakersPictures.add(speakerses.get(counter).getPhoto_url());
+            speakersName.add(speakerses.get(counter).getName());
+            counter++;
+        }
+
+        while(counter!=(speakerses.size())){
+            speakerses2.add(speakerses.get(counter));
+            speakersPictures1.add(speakerses.get(counter).getPhoto_url());
+            speakersName1.add(speakerses.get(counter).getName());
+            counter++;
+        }
+    }
+
+
+
+    public void Updates(){
+        checkforchanges.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(sharedPreferences.getInt("CHANGE_COMMIT",-1)==1){
+                   runOnUiThread(new Runnable() {
+                       @Override
+                       public void run() {
+                           ClearArrays();
+                           fetchData();
+                           setSpeakerListViews();
+                           editor.putInt("CHANGE_COMMIT",0).commit();
+                       }
+                   });
+
+                }
+            }
+        },0,10000);
+    }
+
+    public void ClearArrays(){
+        speakersName.clear();
+        speakersName1.clear();
+        speakersPictures.clear();
+        speakersPictures1.clear();
+    }
 
 }
